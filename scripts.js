e617ca3d5d04dfb95174bf038c4385e9d3f67ab6
Dan Hono <stelveflame@gmail.com>
2024-03-18 03:52:30 +0400

    Add a function that gets AI's choice

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-console.log('Internal JS file linked');
+// RPS thingy
+// 0 = R
+// 1 = P
+// 2 = S
+
+// 0 + 1 = 1 | R loses P
+// 0 + 2 = 2 | R wins S
+// 1 + 0 = 1 | P wins R
+// 1 + 2 = 3 | P loses S
+// 2 + 0 = 2 | S loses R
+// 2 + 1 = 3 | S wins P
+
+function getComputerChoice() {
+    return Math.floor(Math.random() * 4);
+} 
