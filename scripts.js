4f724b2f50427b5b22ed4f7f78346d23caba94eb
Dan Hono <stelveflame@gmail.com>
2024-03-18 06:28:31 +0400

    Create a function that plays a single round of the game

--- scripts.js
+++ scripts.js
@@ -1,15 +1,34 @@
-// RPS thingy
-// 0 = R
-// 1 = P
-// 2 = S
+// 0 = R; 1 = P; 2 = S
 
-// 0 + 1 = 1 | R loses P
-// 0 + 2 = 2 | R wins S
-// 1 + 0 = 1 | P wins R
-// 1 + 2 = 3 | P loses S
-// 2 + 0 = 2 | S loses R
-// 2 + 1 = 3 | S wins P
-
+// Get computer's choice
 function getComputerChoice() {
     return Math.floor(Math.random() * 4);
-} 
+}
+
+// Play a single round: 0 = draw; 1 = 1p; 2 = 2p
+function playRound(firstChoice, secondChoice) {
+    // Check for draw
+    if (firstChoice === secondChoice) {
+        return 0;
+    }
+
+    // It's important to know which number is larger to determine the winner of the combination
+    let firstLarger = firstChoice > secondChoice ? true : false;
+    switch (firstChoice + secondChoice) {
+        case 1:
+            if (firstLarger) {
+                return 1;
+            }
+            return 2;
+        case 2:
+            if (firstLarger) {
+                return 2;
+            }
+            return 1;
+        case 3:
+            if (firstLarger) {
+                return 1;
+            }
+            return 2;
+    }
+}
