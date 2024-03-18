01483c9a1dd63211adeeafd9373f3e12fe85fb2f
Dan Hono <stelveflame@gmail.com>
2024-03-19 02:30:09 +0400

    Create a function that gets user's choice

--- scripts.js
+++ scripts.js
@@ -1,8 +1,47 @@
-// 0 = R; 1 = P; 2 = S
+// 0 = R; 1 = P; 2 = S (functions that retrieve choices always return one of these numbers!)
 
 // Get computer's choice
 function getComputerChoice() {
     return Math.floor(Math.random() * 4);
+}
+
+// Get player's choice: words, emojis (any style: punctuation and/or -case)
+function getPlayerChoice(answer) {
+    // Ask the user for the input (if none was in the function's argument)
+    if (!answer) {
+        answer = prompt('Pick between water, plank and fire!', 'Fire!');
+    }
+
+    // Still no answer? Force them to make one
+    if (!answer) {
+        alert('Please, make a choice 🙏🏻');
+        return getPlayerChoice();
+    }
+
+    // Retrieve booleans through regex to know which words exist in the answer
+    let water = /(water)|[💦🔫🌊💧]/imu;
+    let plank = /(plank)|🪵/imu;
+    let fire = /(fire)|🔥/imu;
+
+    water = water.test(answer);
+    plank = plank.test(answer);
+    fire = fire.test(answer);
+
+    // Retrieve a choice indefinitely only until the user inputs 1 choice only
+    if (water + plank + fire > 1) {
+        alert('Please, pick only ONE choice');
+        return getPlayerChoice();
+    }
+
+    // Return a number corresponding to the choice made
+    switch (true) {
+        case water:
+            return 0;
+        case plank:
+            return 1;
+        case fire:
+            return 2;
+    }
 }
 
 // Play a single round: 0 = draw; 1 = 1p; 2 = 2p
